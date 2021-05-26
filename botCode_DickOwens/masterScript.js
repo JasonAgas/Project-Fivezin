460b93ba22198175bcfede4c9c4d124d4c3cdce3
JasonAgas <84216782+JasonAgas@users.noreply.github.com>
2021-05-25 22:20:53 -0700

    May 25th, 2021

--- botCode_DickOwens/masterScript.js
+++ botCode_DickOwens/masterScript.js
@@ -19,7 +19,16 @@ client.login(token);
 
 /* Message Commands */
 client.on('message', message => {
-    if (message.content.startWith('dick')) {
+    if (message.content === (`${prefix}commands`)) {
+        message.channel.send(`The current commands I can respond to are:\n
+        !dick --> You'll see :) \n
+        !clothes --> I will tell you how much I love clothes.
+        !server --> I will tell you the server name and number of members. \n
+        !serverDate --> I will tell you the date that the server was created. \n
+        !region --> I will tell you the region that this server is based in.\n
+        That is all. Stay tuned in for more commands! :)`)
+     }
+    else if (message.content.startWith('dick')) {
 		message.channel.send(`You call?`);
 	}
     else if (message.content === `${prefix}server`) {
