eaee6e4b5014ecfda9ae878a6fed7f885e4a34b7
JasonAgas <84216782+JasonAgas@users.noreply.github.com>
2021-06-03 22:39:03 -0700

    Delete commands/random directory

--- commands/random/clothes.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name:'clothes',
-    description: 'Thoughts on clothes',
-    execute(message,args){
-        message.channel.send(`Fuck off me`);
-    },
-};
--- commands/random/dick.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'dick',
-    description: 'The name',
-    execute(message, args){
-        message.channel.send(`You called me?`);
-    },
-};
