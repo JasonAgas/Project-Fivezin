dd14e69aff552cad9bba88bbd0089f5d1789eb04
JasonAgas <84216782+JasonAgas@users.noreply.github.com>
2021-06-03 22:39:14 -0700

    Delete commands/technical directory

--- commands/technical/region.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'region',
-    descritpion: 'Where are we?',
-    execute(message, args){
-        message.channel.send(`This server is based in: ${message.guild.region}` )
-    },
-};
--- commands/technical/serverdate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'serverdate',
-    description: 'What day is it?',
-    execute(message, args){
-        message.channel.send(`This server was created on: ${message.guild.createdAt}`)
-    },
-};
