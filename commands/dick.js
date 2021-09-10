4e40b0696e5c76c449b444a238a2dc1268960cda
Jason Agas <84216782+JasonAgas@users.noreply.github.com>
2021-09-10 15:46:44 -0700

    Delete dick.js

--- commands/dick.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'dick',
-    description: 'The bot will respond to its name being called',
-    execute(message,args) {
-        message.channel.send(`You call?`);
-    }
-}
